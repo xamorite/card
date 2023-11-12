14845d5e52e4025a57cba2135f471b263dff3d0a
Xamorite <eogunneye@gmail.com>
2023-11-12 21:37:13 +0100

    card application

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+
+             child: Column(
+                children: [
+                  CircleAvatar(
+                    radius:50.0,
+                     backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/112722566?v=4'),
+
+                  )
+                  ,Text(
+                    'Xamorite',
+                     style:TextStyle(
+                       fontSize: 40.0,
+                       fontFamily: 'Pacifico',
+                       color:Colors.white,
+                       fontWeight: FontWeight.bold,
+                     ),
+                  ),
+                  Text(
+                      'SOFTWARE DEVELOPER',
+                  style:TextStyle(
+                    fontSize: 20.0,
+                    color:Colors.teal.shade100,
+                    fontFamily: 'SourceSans3-VariableFont_wght.ttf',
+                    letterSpacing: 2.5,
+                  ),
+                  ),
+                ]
+             )
+        ),
       ),
     );
   }
